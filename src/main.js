ef830ed1f91b63e3c879a722c5c5599ee6869ed9
afifahamza <afifa.n.341@gmail.com>
2024-05-14 23:59:56 +0500

    added assets and old code

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-Vue.config.productionTip = false
+import "./assets/css/global.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
